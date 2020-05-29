9f5a5e4a20cddd74785ffcaddf436b64b2608fd8
Fabi <fabperaltar@gmail.com>
2020-05-29 16:46:23 -0500

    TFTP put

--- TFTP/client.py
+++ TFTP/client.py
@@ -1,137 +1,158 @@
-import sys, struct, tftp, socket, logging
+import sys, struct, tftp, socket, logging, errno, os.path
+from os import path
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s',)
+logging.basicConfig(level=logging.DEBUG, filename="tftp.log", format='%(asctime)s %(message)s',)
 
 class client():
-
-	START = 0
-	DATA = 1
+	
+	server_addr = ("127.0.0.1", 69)
     
-	def __init__(self, host="127.0.0.1", port=69, filename=None):
+	def __init__(self, host="127.0.0.1", port=69):
 		self.server_addr = (host, port)
-		self.block_num = 1
-		self.is_done = False
-		self.status = self.START
-		self.action = ""
-		self.block_size = 512
-		self.filename = filename
-		self.setup_file()
-		self.setup_connect()
 
-	def reset(self):
-		self.block_num = 1
-		self.is_done = False
-		self.status = self.START
-		self.port = 69
-		self.setup_file()
-		self.setup_connect()
+	# def reset(self):
+	# 	self.block_num = 1
+	# 	self.is_done = False
+	# 	# self.status = self.START
+	# 	self.port = 69
+	# 	# self.setup_file()
+	# 	self.setup_connect()
 
-	def max_packet_size(self):
-		return self.block_size + 4
+	# def max_packet_size(self):
+	# 	return 516
 
-	def setup_connect(self):
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# def setup_connect(self):
+	# 	self.sock_fd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-	def send_packet(self, packet):
-		self.sock.sendto(packet, self.server_addr)
+	# def send_packet(self, packet):
+	# 	self.sock_fd.sendto(packet, self.server_addr)
 
-	def recv_packet(self):
-		(packet, addr) = self.sock.recvfrom(self.max_packet_size)
-		return (packet, addr)
+	# def recv_packet(self):
+	# 	(packet, addr) = self.sock_fd.recvfrom(self.max_packet_size)
+	# 	return (packet, addr)
 
 	def print_logging(self, msg):
 		logging.debug(msg)
+	
+	def put(self, filename):
+		try:
+			with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as udp_socket:
+				self.print_logging("Ready to send %s" % filename)
+				wrq_packet = tftp.set_request_packet(tftp.WRQ, filename)
+				self.print_logging("Setting request packet...")
+				udp_socket.sendto(wrq_packet, self.server_addr)
+				self.print_logging("WRQ sent")
+				ack_packet = udp_socket.recvfrom(516)
+				if(tftp.get_opcode(ack_packet) == tftp.ACK and tftp.get_blocknum(ack_packet) == 0):
+					self.print_logging("Received ACK for %d" % 0)
+					with open(filename, "rb") as fd:
+						block_num = 1
+						while(True):
+							data = fd.read(512)
+							packet = tftp.set_data_packet(block_num, data)
+							udp_socket.sendto(packet, self.server_addr)
+							ack_packet = udp_socket.recvfrom(516) 
+							# if(tftp.get_opcode(ack_packet) == block_num):
+							self.print_logging("Received ACK for %d" % block_num)
+							block_num += 1
+							if(not data):
+								break
+					packet = tftp.set_data_packet(block_num, data)
+		except Exception as e:
+			self.print_logging(e)
+		# except OSError as e:
+		# 	if(e.errno == errno.ENONET):
+		# 		# file not found
+		# 	elif(e.errno == errno.EPERM or errno.EACCES):
+		# 		# acces violation
+		# 	elif(errno == errno.EFBIG or errno.ENOSPC):
+		# 		# disk full
+		# 	else:
+		# 		# unknown
+			
 
-	def setup_file(self):
-		if(self.filename):
-			if(self.action == "get"):
-				self.fd = open(self.filename, 'wb')
-			elif(self.action == "put"):
-				self.fd = open(self.filename, 'rb')
-			else:
-				raise Exception("Unsupport action %s" % self.action)
+	def get(self, filename):
+		try:
+			pass
+		except Exception as e:
+			print(e)
 
-	def handle_packet(self, packet, addr):
-		host, port = addr
-		if(host != self.server_addr[1]):
-			return
+	# def handle_packet(self, packet, addr):
+	# 	host, port = addr
+	# 	if(host != self.server_addr[1]):
+	# 		return
 
-		packet_len = len(packet)
-		opcode = tftp.get_opcode(packet)
+	# 	packet_len = len(packet)
+	# 	opcode = tftp.get_opcode(packet)
 
-		if(opcode == tftp.ERROR):
-			err_code = struct.unpack("!H", packet[2:4])[0]
-			err_msg = packet[4:packet_len-1]
-			print("Error %s: %s" % (err_code, err_msg))
-			sys.exit(err_code)
+	# 	if(opcode == tftp.ERROR):
+	# 		err_code = struct.unpack("!H", packet[2:4])[0]
+	# 		err_msg = packet[4:packet_len-1]
+	# 		print("Error %s: %s" % (err_code, err_msg))
+	# 		sys.exit(err_code)
 
-		elif(opcode == tftp.DATA):
-			if(self.port != port):
-				self.port = port
-			block_num = struct.unpack("!H", packet[2:4])[0]
-			if(block_num != self.block_num):
-				print("Unexpected block num %d" % block_num)
-				return
-			data = packet[4:]
-			self.fd.write(data)
-			if(len(packet) < self.block_size + 2):
-				self.is_done = True
-				self.fd.close()
-				file_len = self.block_size * (self.block_num -1) + len(data)
-				print("%d bytes received." % file_len)
-			self.block_num += 1
+	# 	elif(opcode == tftp.DATA):
+	# 		if(self.port != port):
+	# 			self.port = port
+	# 		block_num = struct.unpack("!H", packet[2:4])[0]
+	# 		if(block_num != self.block_num):
+	# 			print("Unexpected block num %d" % block_num)
+	# 			return
+	# 		data = packet[4:]
+	# 		self.fd.write(data)
+	# 		if(len(packet) < self.block_size + 2):
+	# 			self.is_done = True
+	# 			self.fd.close()
+	# 			file_len = self.block_size * (self.block_num -1) + len(data)
+	# 			print("%d bytes received." % file_len)
+	# 		self.block_num += 1
 		
-		elif(opcode == tftp.ACK):
-			if self.port != port:
-				self.port = port
-			block_num = struct.unpack("!H", packet[2:4])[0]
-			self.print_logging("Received ACK for %d" % block_num)
-			self.block_num += 1
+	# 	elif(opcode == tftp.ACK):
+	# 		if self.port != port:
+	# 			self.port = port
+	# 		block_num = struct.unpack("!H", packet[2:4])[0]
+	# 		self.print_logging("Received ACK for %d" % block_num)
+	# 		self.block_num += 1
 
-		else:
-			raise Exception("Unrecognized packet: %s", str(opcode))
+	# 	else:
+	# 		raise Exception("Unrecognized packet: %s", str(opcode))
         
-	def get_next_packet(self):
-		if(self.status == self.START):
-			opcode = tftp.RRQ if self.action == "get" else tftp.WRQ
-			self.print_logging("About to send packet %d" % opcode)
-			packet = tftp.set_request_packet(opcode, self.filename)
-			self.status = self.DATA
-		elif(self.status == self.DATA):
-			if(self.action == "get"):
-				self.print_logging("About to send ACK for %d" % (self.block_num - 1))
-				packet = tftp.set_ack_packet(self.block_num-1)
-			elif(self.action == "put"):
-				self.print_logging("About to send data for %d" % (self.block_num - 1))
-				data = self.fd.read(self.block_size)
-				if(len(data) < self.block_size):
-					self.is_done = True
-				packet = tftp.set_data_packet(self.block_num - 1, data)
+	# def get_next_packet(self):
+	# 	if(self.status == self.START):
+	# 		opcode = tftp.RRQ if self.action == "get" else tftp.WRQ
+	# 		self.print_logging("About to send packet %d" % opcode)
+	# 		packet = tftp.set_request_packet(opcode, self.filename)
+	# 		self.status = self.DATA
+	# 	elif(self.status == self.DATA):
+	# 		if(self.action == "get"):
+	# 			self.print_logging("About to send ACK for %d" % (self.block_num - 1))
+	# 			packet = tftp.set_ack_packet(self.block_num-1)
+	# 		elif(self.action == "put"):
+	# 			self.print_logging("About to send data for %d" % (self.block_num - 1))
+	# 			data = self.fd.read(self.block_size)
+	# 			if(len(data) < self.block_size):
+	# 				self.is_done = True
+	# 			packet = tftp.set_data_packet(self.block_num - 1, data)
 
-		return packet
+	# 	return packet
 
-	def handle(self):
-		while(not self.is_done):
-			packet = self.get_next_packet()
-			if(packet):
-				self.send_packet(packet)
-			(packet, addr) = self.recv_packet()
-			self.handle_packet(packet, addr)
+	# def handle(self):
+	# 	while(not self.is_done):
+	# 		packet = self.get_next_packet()
+	# 		if(packet):
+	# 			self.send_packet(packet)
+	# 		(packet, addr) = self.recv_packet()
+	# 		self.handle_packet(packet, addr)
 
 
 tftp_client = client()					
 
-def get(filename):
-	tftp_client.filename = filename
-	tftp_client.action = "get"
-	tftp_client.reset()
-	tftp_client.handle()
+# def get(filename):
+# 	tftp_client.filename = filename
+# 	tftp_client.action = "get"
+# 	tftp_client.reset()
+# 	tftp_client.handle()
 
-def put(filename):
-	tftp_client.filename = filename
-	tftp_client.action = "put"
-	tftp_client.reset()
-	tftp_client.handle()
 
 
 print("TFTP client.")
@@ -140,12 +161,12 @@ while True:
 	command = line.split(" ")
 	if(command[0] == "connect"):
 		print(command)
-		# tftp_client.server_addr = (command[1], command[2])
+		tftp_client.server_addr = (command[1], int(command[2]))
 	elif(command[0] == "get"):
 		print(command)
-		# get(command[1])
+		# tftp_client.get(command[1])
 	elif(command[0] == "put"):
 		print(command)
-		# put(command[1])
+		tftp_client.put(command[1])
 	elif(command[0] == "quit"):
 		break
--- TFTP/server.py
+++ TFTP/server.py
@@ -1,16 +1,94 @@
-import socket
+import sys, struct, tftp, socket, logging, errno, os.path
+from os import path
 
-MAXSIZE = 500
-PORT = 69
+logging.basicConfig(level=logging.DEBUG, filename="tftp.log", format='%(asctime)s %(message)s',)
 
-def main():
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.bind(("", PORT))
-	print("listening on {}".format(PORT))
+class server():
 
-	while True:
-		data, addr = s.recvfrom(MAXSIZE)
-		print("{} from {}".format(data, addr))
-		s.sendto("Hello, {}".format(addr), addr)
+	addr = ("127.0.0.1", 69)
 
-main()
+	def __init__(self, host="127.0.0.1", port=69):
+		self.addr = (host, port)
+
+		with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as udp_client:
+			while True:
+				print("Listening...")
+				self.print_logging("Listening...")
+				rq_packet, client_addr = udp_client.recvfrom(512)
+				rq_code = tftp.get_opcode(rq_packet)
+				if(rq_code == 1):
+					# RRQ
+					pass
+				elif(rq_code == 2):
+					# WRQ
+					self.print_logging("Write request received from {}".format(client_addr))
+					self.put(rq_packet, udp_client, client_addr)
+				else:
+					self.print_logging("Unknown request")
+
+	def put(self, rq_packet, udp_client, client_addr):
+		try:
+			if(tftp.file_exist(tftp.get_filename(rq_packet))):
+				packet = tftp.set_error_packet(6, "File already exists")
+				udp_client.sendto(packet, client_addr)
+				return
+			else:
+				ack = tftp.set_ack_packet(0)
+				udp_client.sendto(ack, client_addr)
+				with open(tftp.get_filename(rq_packet), "wb") as fd:
+					while(True):
+						packet = udp_client.recvfrom(516)
+						data = packet[4:]
+						if(not data):
+							break
+						else:
+							fd.write(data)
+							ack_no = tftp.get_blocknum(packet)
+							ack_packet = tftp.set_ack_packet(ack_no)
+							udp_client.sendto(ack_packet, client_addr)
+
+
+
+
+		except OSError as e:
+			if(e.errno == errno.ENOENT):
+				# file not found
+				packet = tftp.set_error_packet(1, "File not found")
+				udp_client.sendto(packet, client_addr)
+				return 
+			elif(e.errno == errno.EPERM or errno.EACCES):
+				# acces violation
+				packet = tftp.set_error_packet(2, "Access violation")
+				udp_client.sendto(packet, client_addr)
+				return
+			elif(errno == errno.EFBIG or errno.ENOSPC):
+				# disk full
+				packet = tftp.set_error_packet(3, "Disk full")
+				udp_client.sendto(packet, client_addr)
+				return
+			else:
+				# unknown
+				packet = tftp.set_error_packet(0, "Unknown")
+				udp_client.sendto(packet, client_addr)
+				return
+		except Exception as e:
+			self.print_logging(e)
+			return
+
+	def print_logging(self, msg):
+		logging.debug(msg)
+				
+
+
+
+# def main():
+# 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 	s.bind(("", PORT))
+# 	print("listening on {}".format(PORT))
+
+# 	while True:
+# 		data, addr = s.recvfrom(MAXSIZE)
+# 		print("{} from {}".format(data, addr))
+# 		s.sendto("Hello, {}".format(addr), addr)
+
+server("192.168.1.68", 69)
