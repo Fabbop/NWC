e1715996885949be8e1724b19cb4fbb1f272d01f
Fabi <fabperaltar@gmail.com>
2020-05-30 16:45:08 -0500

    RPC file system

--- RPC/client.py
+++ RPC/client.py
@@ -17,5 +17,50 @@ while(True):
 		print("User not found")
 
 while(True):
-	line = input(">")
-	command = line.split(" ")
+	try:
+		line = input(client.cwd + " : ")
+		command = line.split(" ")
+
+		if(command[0] == "cd"):
+			msg = client.server.change_directory(command[1], client.cwd, client.user_dir)
+			if(msg == "You can't change directory" or msg == "No such file or directory"):
+				print(msg)
+			else:
+				if(command[1] == ""):
+					client.cwd = client.user_dir
+				else:
+					client.cwd = client.server.change_directory(command[1], client.cwd, client.user_dir)
+
+		elif(command[0] == "mkdir"):
+			print(client.server.mkdir(client.cwd, command[1]))
+
+		elif(command[0] == "rmdir"):
+			print(client.server.rmdir(client.cwd, command[1]))
+
+		elif(command[0] == "ls"):
+			for x in client.server.ls(client.cwd):
+				print(x)
+
+		elif(command[0] == "remove"):
+			print(client.server.delete_file(client.cwd, command[1]))
+
+		elif(command[0] == "create"):
+			print(client.server.create_file(client.cwd, command[1]))
+
+		elif(command[0] == "write"):
+			data = " ".join(command[2:])
+			print(client.server.write_to_file(client.cwd, command[1], data))
+		
+		elif(command[0] == "read"):
+			lines = client.server.read_file(client.cwd, command[1])
+			for i in lines:
+				print(i)
+
+		elif(command[0] == "quit"):
+			break
+
+		else:
+			print("Command not found")
+		
+	except Exception as e:
+		print(e)
--- RPC/server.py
+++ RPC/server.py
@@ -1,36 +1,107 @@
-import os
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+import os
 
-# Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
 	rpc_paths = ("/RPC2",)
 	
-# Create server
-with SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler) as server:
+with SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler, allow_none=True, logRequests=True) as server:
   
 	server.register_introspection_functions()
 
-	# Register a function under a different name
-	# def adder_function(x, y):
-	# 	return x + y
-
-	# server.register_function(adder_function, 'add')
-
-	# Register an instance; all the methods of the instance are
-	# published as XML-RPC methods (in this case, just 'mul').
 	class FileSystem:
 
 		def check_directory(self, directory):
 			return os.path.exists(directory)
 
-		def create_file(self, filename, fnext):
-			pass
+		def mkdir(self, path, namedir):
+			dirpath = path + namedir
+			try:
+				os.mkdir(dirpath)
+			except OSError as e:
+				msg = "An Error has ocurred: %s" % e
+				return msg
+			else:
+				msg = "Directory %s created" % dirpath
+				return msg
 
-		def mul(self, x, y):
-			return x * y
+		def rmdir(self, path, namedir):
+			dirpath = path + namedir
+			try:
+				os.rmdir(dirpath)
+			except OSError as e:
+				msg = "An Error has ocurred: %s" % e
+				return msg
+			else:
+				msg = "Directory %s deleted" % dirpath
+				return msg
+
+		def delete_file(self, path, filename):
+			filepath = path + filename
+			try:
+				os.remove(filepath)
+			except OSError as e:
+				msg = "An Error has ocurred: %s" % e
+				return msg
+			else:
+				msg = "File %s deleted" % filepath
+				return msg
+
+		def create_file(self, cwd, filename):
+			filepath = cwd + filename + ".txt"
+			try:
+				f = open(filepath, "wb")
+				f.close()
+			except Exception as e:
+				msg = "An Error has ocurred: %s" % e
+				return msg
+			else:
+				msg = "File %s created" % filename
+				return msg
+
+		def write_to_file(self, cwd, filename, data):
+			filepath = cwd + filename
+			try:
+				with open(filepath, "a") as fd:
+					fd.write(data + "\r\n")
+			except Exception as e:
+				msg = "An Error has ocurred: %s" % e
+				return msg
+			else:
+				msg = "File written"
+				return msg
+
+		def read_file(self, cwd, filename):
+			filepath = cwd + filename
+			try:
+				with open(filepath, "r") as fd:
+					lines = fd.readlines()
+			except Exception as e:
+				msg = "An Error has ocurred: %s" % e
+				return msg
+			else:
+				return lines
+
+		def ls(self, path):
+			return os.listdir(path)
+		
+		def change_directory(self, path, cwd, user_dir):
+			if(path == ".."):
+				if(cwd == user_dir):
+					return "You can't change directory"
+				else:
+					current = cwd.split("/")[:-2]
+					cwd = "/".join(current) + "/"
+					return cwd
+			elif(self.check_directory(cwd + path)):
+				cwd = cwd + path + "/"
+				return cwd
+			elif(path == ""):
+				return user_dir
+			else:
+				return "No such file or directory"
+
 
 	server.register_instance(FileSystem())
 
-	# Run the server's main loop
 	server.serve_forever()
